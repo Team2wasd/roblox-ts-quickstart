e13df96749a1b590bdbe9c4be14a404e9f608b36
IRoblian <sonan0721@gmail.com>
2022-10-10 23:10:12 +0900

    set default logger

--- src/server/runtime.server.ts
+++ src/server/runtime.server.ts
@@ -1,4 +1,16 @@
-import { Flamework } from "@flamework/core";
+import { Flamework, Modding } from "@flamework/core";
+import Log, { Logger } from "@rbxts/log";
+
+Log.SetLogger(
+    Logger.configure()
+        .WriteTo(Log.RobloxOutput()) // WriteTo takes a sink and writes to it
+        .EnrichWithProperty("Version", _VERSION) // Will add "Version" to the event data
+        .Create()
+);
+
+Modding.registerDependency<Logger>(ctor => {
+    return Log.ForContext(ctor); // will register this under the given DI class
+});
 
 Flamework.addPaths("src/server/components");
 Flamework.addPaths("src/server/services");
